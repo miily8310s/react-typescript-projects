d8034a852f3acd64d59d2994dac5a8b4cae56cb6
miily8310s <rillakerika@gmail.com>
2021-06-24 23:37:47 +0900

    Refactor Action type definition

--- redux-ts/src/state/reducers/repositoriesReducer.ts
+++ redux-ts/src/state/reducers/repositoriesReducer.ts
@@ -1,22 +1,23 @@
+import { ActionType } from '../action-types';
+import { Action } from '../actions';
+
 interface RepositoriesStete {
   loading: boolean;
   error: string | null;
   data: string[];
 }
 
-interface Action {
-  type: string;
-  payload: string[];
-}
-
-const reducer = (state: RepositoriesStete, action: Action) => {
+const reducer = (
+  state: RepositoriesStete,
+  action: Action
+): RepositoriesStete => {
   switch (action.type) {
-    case 'search_repositories':
+    case ActionType.SEARCH_REPOSITORIES:
       return { loading: true, error: null, data: [] };
-    case 'search_repositories_success':
+    case ActionType.SEARCH_REPOSITORIES_SUCCESS:
       return { loading: true, error: null, data: action.payload };
-    case 'search_repositories_error':
-      return { loading: true, error: null, data: [] };
+    case ActionType.SEARCH_REPOSITORIES_ERROR:
+      return { loading: true, error: action.payload, data: [] };
     default:
       return state;
   }
