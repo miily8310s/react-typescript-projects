f3fc19e17312b93c234c56a0c93adaa94278554f
miily8310s <rillakerika@gmail.com>
2021-06-27 17:12:27 +0900

    add onClick and onChange to index.tsx and wasmfile

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
-  return <h1>Hi!</h1>
+  const [input, setInput] = useState('')
+  const [code, setCode] = useState('')
+
+  const onClick = () => {
+    console.log(input)
+  }
+  return (
+    <div>
+      <textarea value={input} onChange={(e) => setInput(e.target.value)}></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre></pre>
+    </div>
+  )
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
