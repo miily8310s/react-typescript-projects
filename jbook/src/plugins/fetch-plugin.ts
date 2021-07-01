7e853dce004b271b6ae1e2aaa8d1bb8ff3ba75ad
miily8310s <rillakerika@gmail.com>
2021-07-01 09:05:32 +0900

    Add css file output

--- jbook/src/plugins/fetch-plugin.ts
+++ jbook/src/plugins/fetch-plugin.ts
@@ -11,8 +11,6 @@ export const fetchPlugin = (inputCode: string) => {
     name: 'fetch-plugin',
     setup(build: esbuild.PluginBuild) {
     build.onLoad({ filter: /.*/ }, async (args: any) => {
-      console.log('onLoad', args);
-  
       if (args.path === 'index.js') {
         return {
           loader: 'jsx',
@@ -20,21 +18,35 @@ export const fetchPlugin = (inputCode: string) => {
         };
       }
   
-      const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
-        args.path
-      );
+      // const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+      //   args.path
+      // );
   
-      if (cachedResult) {
-        return cachedResult;
-      }
+      // if (cachedResult) {
+      //   return cachedResult;
+      // }
   
       const { data, request } = await axios.get(args.path);
+
+      const fileType = args.path.match(/.css$/) ? 'css' : 'jsx'
+
+      const escaped = data
+        .replace(/\n/g, '')
+        .replace(/"/g, '\\"')
+        .replace(/'/g, "\\'")
+      const contents = 
+        fileType === 'css' 
+        ? `
+        const style = document.createElement('style');
+        style.innerText = '${escaped}';
+        document.head.appendChild(style);
+      ` : data
+
       const result: esbuild.OnLoadResult = {
         loader: 'jsx',
-        contents: data,
+        contents,
         resolveDir: new URL('./', request.responseURL).pathname,
       };
-  
       await fileCache.setItem(args.path, result);
   
       return result;
